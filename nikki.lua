26074267adfa77aa73fb5aa6c0c0a957dba28ac8
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-09-15 14:40:30 +0700

    Update nikki.lua

--- nikki.lua
+++ nikki.lua
@@ -17,7 +17,7 @@ function action_backup()
     local date = os.date("%Y-%m-%d_%H-%M-%S")
     local backup_file = "/tmp/nikki_backup-" .. date .. ".tar.gz"
 
-    sys.call("tar -czf " .. backup_file .. " /etc/config/nikki /etc/nikki/profiles /etc/nikki/run >/dev/null 2>&1")
+    sys.call("tar -czf " .. backup_file .. " /etc/config/nikki /etc/nikki/profiles /etc/nikki/run /etc/nikki/subscriptions >/dev/null 2>&1")
 
     http.header('Content-Disposition', 'attachment; filename="' .. fs.basename(backup_file) .. '"')
     http.prepare_content("application/gzip")
